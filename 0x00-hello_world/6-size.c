be9ef111580e92ddeab7ceb7c3c7ef8f5b69b8cb
yassinebenabdesslam <yassintshirtcreator@gmail.com>
2020-06-10 14:33:51 +0100

    espace

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a;
+int z;
+long int e;
+long long int r;
+float t;
+printf("%d", sizeof(a));
+printf("%d", sizeof(z));
+printf("%d", sizeof(e));
+printf("%d", sizeof(r));
+printf("%d", sizeof(t));
+return (0);
+}
