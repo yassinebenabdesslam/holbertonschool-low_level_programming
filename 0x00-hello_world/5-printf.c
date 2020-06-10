8b6ea5a4aa666bea152681359f71e67870a5277e
yassinebenabdesslam <yassintshirtcreator@gmail.com>
2020-06-10 14:09:27 +0100

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 /**
 *main -pulp faction
+*
 *Return: 0
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,/n");
 return (0);
 }
