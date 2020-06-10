c4c88eaa9f9213d2908d2f9ce269e043b41dffb1
yassinebenabdesslam <yassintshirtcreator@gmail.com>
2020-06-10 13:19:07 +0100

    int main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
 *main -pulp faction
 *Return: 0
 */
-int main(void){
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
