056cc0e6965b2590adc32d888b6b42f6f2e68dd9
yassinebenabdesslam <yassintshirtcreator@gmail.com>
2020-06-11 12:30:02 +0100

    lol

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-  printf("%d is positive\n",n);
+printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-  printf("%d is zero\n",n);
+printf("%d is zero\n", n);
 }
 else
 {
-  print("%d is negative",n);
+print("%d is negative", n);
 }
 return (0);
 }
