07a371beaa367e3e82165c47646628e664c0b6fd
yassinebenabdesslam <yassintshirtcreator@gmail.com>
2020-06-16 10:50:23 +0100

    @n int

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_sign - writes the character c to stdout
+ * @n: int c
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else
+{
+_putchar('0');
+return (0);
+}
+}
