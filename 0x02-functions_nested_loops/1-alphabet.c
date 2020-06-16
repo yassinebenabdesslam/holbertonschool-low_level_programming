ae33d5adc003e28bf2cbc0f570eb138bbc4fb086
yassinebenabdesslam <yassintshirtcreator@gmail.com>
2020-06-16 10:04:44 +0100

    lol

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,15 +5,13 @@
  *
  * Return: Always 0.
  */
+
 void print_alphabet(void)
 {
-
 int i;
-
-for (i = 'a'; i = 'z'; i++)
+for (i = 'a'; i <= 'z'; i++)
 {
 _putchar(i);
-_putchar("\n");
 }
-return (0);
+_putchar('\n');
 }
