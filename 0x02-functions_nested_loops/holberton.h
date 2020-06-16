5c8fcd0ee984ee1d9ebc674be3812a9197b2500e
yassinebenabdesslam <yassintshirtcreator@gmail.com>
2020-06-16 09:37:38 +0100

    The header

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,12 +1,15 @@
-#include "holberton.h"
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <stdio.h>
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
 
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-  print_alphabet();
-  return (0);
-}
+int add(int, int);
+#endif
