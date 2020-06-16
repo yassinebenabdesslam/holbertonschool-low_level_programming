8999af0422fae4aeabe4cf261f69b115324418f2
yassinebenabdesslam <yassintshirtcreator@gmail.com>
2020-06-16 10:59:35 +0100

    @n : int n

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - check the code for Holberton School students.
+ *@n : int n
+ * Return: Always 0.
+ */
+
+int print_last_digit(int n)
+{
+  
+if ( n < 0)
+
+    n = ((-1) * (n % 10));
+
+else
+
+n = ( n % 10);
+_putchar(n + '0');
+return (n);
+
+}
