bf68f55836ec78388b95ca234706d2fe9da01fb6
yassinebenabdesslam <yassintshirtcreator@gmail.com>
2020-06-16 16:07:50 +0100

    lol

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,28 +7,32 @@
  */
 void print_to_98(int n)
 
+
 {
 int  i;
 for (i = n; i <= 98; i++)
 {
 if (i >= 10)
 {
+if (i != 0)
+{
+_putchar(',');
+_putchar(' ');
+}
 _putchar(i / 10 + '0');
 _putchar(i % 10 + '0');
+
+}
+else
+{
+if (i != n)
+{
 _putchar(',');
 _putchar(' ');
 }
-else if (i < 10)
-{
 _putchar(i);
-_putchar(',');
-_putchar(' ');
 }
-else if (i != n)
-{
-_putchar(',');
-_putchar(' ');
-}
-}
+
 putchar('\n');
+}
 }
